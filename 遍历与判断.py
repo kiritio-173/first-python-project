393e7a537d4eeaf4e6537f6e302e5448f6efb216
kiritio-173 <61039168+kiritio-173@users.noreply.github.com>
2021-08-26 23:41:37 +0800

    Add files via upload

---
+++ 遍历与判断.py
@@ -0,0 +1,14 @@
+# 日期 : 2021/8/26
+# 时间 : 20:23
+# 作者 : kiritio
+print('p' in 'python')#true
+print('k' in 'python')#false
+
+lst=[10,20,'ok','python']
+print('l'in lst)#f
+
+print(10 in lst)#T；not in 为F
+print('-----------------------------------------')
+
+for i in lst:
+    print(i)
