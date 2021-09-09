f99fd100b597a5ef90ca6c9c05d67b16f962841c
kiritio-173 <61039168+kiritio-173@users.noreply.github.com>
2021-09-09 16:42:37 +0800

    Update 传递任意数量的实参.py

--- 函数/传递任意数量的实参.py
+++ 函数/传递任意数量的实参.py
@@ -2,7 +2,7 @@
 # 时间 : 11:01
 # 作者 : kiritio
 def make_pizza(size,*toppings):
-    '''介绍索要制作的披萨'''
+    '''介绍所要制作的披萨'''
     print("\nMaking a "+str(size)+"-inch pizza with the following toppings:")
     for topping in toppings:
         print('-'+topping)
